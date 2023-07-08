b68e12b9674d6f446e67d1c115ae91aa13af3fc6
Rahul <rahulscodingworld@gmail.com>
2023-07-08 18:02:35 +0530

    added a tree

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,19 @@ export default function Home() {
 
 
   return (
-    <div className='flex flex-col flex-nonwrap place-content-center items-center h-screen overflow-auto p-0 relative gap-2 drop-shadow'>
+    <div className='flex flex-row gap-10 flex-nonwrap place-content-center items-center h-screen overflow-auto p-0 relative drop-shadow'>
       <Head>
         <title>Get Back to Work!</title>
       </Head>
+      <div>
         <div>
           Time Left:
           <Countdown date={new Date("Jul 15, 2023")} className='px-2'/>
         </div>
-      <div className='text-xl'>Be Wise and Don&#39;t Get Distracted™️</div>
+      <div className='text-xl pt-3'>Be Wise and Don&#39;t Get Distracted™️</div>
       <div>
-        <button disabled className='mx-2 my-4 text-sm rounded-md bg-orange-300 shadow-xl shadow-orange-300/50 dark:bg-orange-500 p-2 px-4 dark:shadow-orange-500/50 '>Analytics</button>
-        <button className='mx-2 my-4 text-sm rounded-md bg-orange-300 shadow-xl shadow-orange-300/50 dark:bg-orange-500 p-2 px-4 dark:shadow-orange-500/50 ' onClick={changeVid}>Video</button>
+        <button disabled className=' my-4 text-sm rounded-md bg-orange-300 shadow-lg shadow-orange-300/50 dark:bg-orange-500 p-2 px-4 dark:shadow-orange-500/50 '>Analytics</button>
+        <button className='mx-2 my-4 text-sm rounded-md bg-orange-300 shadow-lg shadow-orange-300/50 dark:bg-orange-500 p-2 px-4 dark:shadow-orange-500/50 ' onClick={changeVid}>Video</button>
       </div>
       {
         playVid 
@@ -33,14 +34,15 @@ export default function Home() {
           <source src="bewise.mp4" type="video/mp4"/>
           </video>
           :
-          <span className='flex items-center pt-2'>
+          <span className='flex items-center pt-2 pl-4 text-sm'>
             <div className='text-md pr-2 animate-bounce'>
               Play video!
             </div>
             <img src='arrow.png' className='w-8 h-6 animate-bounce' alt='arrow' />
           </span>
       }
-        
+      </div>
+        <img src='tree.jpg' className='rounded-md shadow-2xl w-1/5 h-1/4' alt='tree' />
    </div>
   )
 }
